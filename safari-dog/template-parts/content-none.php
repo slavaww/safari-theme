a18378c0841160bd0cea5c23a1ed80823645fff0
Viacheslav Rukavichkin <s@u-wr.ru>
2026-03-11 10:44:37 +0300

    Fix display error on index page

--- safari-dog/template-parts/content-none.php
+++ safari-dog/template-parts/content-none.php
@@ -3,6 +3,8 @@
  * If there is not content
  */
 ?>
-<div class="row">
-    <h1>Нет ничего</h1>
+<div class="container safari-index">
+    <div class="row mt-5">
+        <h1>Нет ничего</h1>
+    </div>
 </div>
